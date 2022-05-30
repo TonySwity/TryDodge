2b24d67ba6dc4ca13d40d6a1b168caa2b63d0113
TonySwity <goldocean@live.com>
2022-05-30 23:44:27 +0300

    add BombRotate.cs and method

---
+++ Assets/Scripts/BombRotate.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BombRotate : MonoBehaviour
+{
+    private float _angleZ;
+
+    private void Start()
+    {
+        _angleZ = Random.Range(-90f, 90f);
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0,0, _angleZ * Time.deltaTime);
+    }
+}
