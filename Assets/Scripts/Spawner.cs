f98bf3140c208576cc3ace2379e35caced278f1c
TonySwity <goldocean@live.com>
2022-06-01 00:34:07 +0300

    add methods

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Spawner : MonoBehaviour
+public class Spawner : ObjectPool
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _bombPrefab;
+    
+    private float _leftBound = -2.4f;
+    private float _rightBound = 2.4f;
+
+    private void Start()
     {
-        
+        Initialize(_bombPrefab);
+        StartCoroutine(SpawnObject());
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private IEnumerator SpawnObject()
     {
+        yield return new WaitForSeconds(Random.Range(0.3f, 1f));
         
+        if (TryGetObject(out GameObject bomb))
+        {
+            SetEnemy(bomb, new Vector2(Random.Range(_leftBound, _rightBound),transform.position.y));
+        }
+    
+        StartCoroutine(SpawnObject());
+    }
+
+    private void SetEnemy(GameObject bomb, Vector2 position)
+    {
+        bomb.SetActive(true);
+        bomb.transform.position = position;
     }
 }
