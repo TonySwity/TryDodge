4e1f968547315e10233427b05733671abbc758e9
TonySwity <goldocean@live.com>
2022-05-31 21:36:21 +0300

    create ObjectPool.cs and add methods

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+   [SerializeField] private GameObject _container;
+   [SerializeField] private int _capacity;
+
+   private List<GameObject> _pool = new List<GameObject>();
+
+   protected void Initialize(GameObject prefab)
+   {
+      GameObject spawned = Instantiate(gameObject, _container.transform);
+      spawned.SetActive(false);
+      
+      _pool.Add(spawned);
+   }
+
+   protected bool TryGetObject(out GameObject result)
+   {
+      result = _pool.First(x => x.activeSelf == false);
+      
+      return result != null;
+   }
+}
