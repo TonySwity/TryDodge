e7301229fe505fe5d038cd8717b53289dbb3da52
TonySwity <goldocean@live.com>
2022-06-03 23:01:22 +0300

    add event Died

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    public event UnityAction Died;
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.TryGetComponent(out Bomb bomb))
         {
+            Died?.Invoke();
             Destroy(gameObject);
         }
     }
