737fbb95a644a6f225e06f62c2562d58a6ad3e34
TonySwity <goldocean@live.com>
2022-06-03 22:59:53 +0300

    add Methods Open Close panel

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _panel;
+
+    private readonly Vector3 _position = new Vector3(0f, -3.863f, 0f);
+    private GameObject _player;
+    
+    private void Start()
     {
-        
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenPanel()
     {
-        
+        _panel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ClosePanel()
+    {
+        _panel.SetActive(false);
+        Time.timeScale = 1;
+
+        if (_player == null)
+        {
+            SpawnPlayer();
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    private void SpawnPlayer()
+    {
+        _player = Instantiate(_playerPrefab, _position, quaternion.identity);
     }
 }
