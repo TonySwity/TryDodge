6275ac7fe2bd262389e167fe5800d544f69de10b
TonySwity <goldocean@live.com>
2022-06-05 00:28:13 +0300

    add audioSource

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(AudioSource))]
 public class Player : MonoBehaviour
 {
-    public event UnityAction Died;
+    [SerializeField] private AudioClip[] _audioClips;
+
+    private AudioSource _audio;
+    
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        _audio.clip = _audioClips[0];
+        _audio.loop = true;
+        _audio.Play();
+        
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.TryGetComponent(out Bomb bomb))
         {
-            Died?.Invoke();
-            Destroy(gameObject);
+            PlayDieSound();
+            Time.timeScale = 0;
+            Destroy(gameObject, 1f);
         }
+    }
+
+    private void PlayDieSound()
+    {
+        _audio.Stop();
+        _audio.loop = false;
+        _audio.clip = _audioClips[1];
+        _audio.Play();
     }
 }
