86ac8ce2eef5345544e24f6315e145c6dc52e8fd
TonySwity <goldocean@live.com>
2022-06-05 00:40:39 +0300

    lite ref and pref

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(Animator),typeof(BoxCollider2D), typeof(AudioSource))]
 public class Bomb : MonoBehaviour
 {
-    private AudioSource _audio;
+    private AudioSource _boomSound;
     private Animator _animator;
     private BoxCollider2D _collider;
     private float _delay = 1f;
@@ -14,25 +13,19 @@ public class Bomb : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _collider = GetComponent<BoxCollider2D>();
-        _audio = GetComponent<AudioSource>();
+        _boomSound = GetComponent<AudioSource>();
+        _boomSound.pitch = Random.Range(0.7f, 1.1f);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        _audio.Play();
+        _boomSound.Play();
         StartCoroutine(Bang());
         
-        if (col.gameObject.TryGetComponent(out Player player))
-        {
-            _animator.CrossFade("Boom",0.1f);
-            _collider.enabled = false;
-        }
+        _animator.CrossFade(BombAnimationController.Params.Boom,0.1f);
+        
+        _collider.enabled = false;
 
-        if (col.gameObject.TryGetComponent(out Background ground))
-        {
-            _animator.CrossFade("Boom",0.1f);
-            _collider.enabled = false;
-        }
     }
 
     private IEnumerator Bang()
