bf9d6e954e07660d5f5a16040d3be07a9ff9b2a7
TonySwity <goldocean@live.com>
2022-06-05 00:27:35 +0300

    create script params Bomb and Player animations

---
+++ Assets/Animations/Bomb/BombAnimationController.cs
@@ -0,0 +1,9 @@
+
+public static class BombAnimationController 
+{
+    public static class Params
+    {
+        public const string Idle = "Idle";
+        public const string Boom = "Boom";
+    }
+}
---
+++ Assets/Animations/Player/PlayerAnimationController.cs
@@ -0,0 +1,8 @@
+public static class PlayerAnimationController
+{ 
+    public static class Params
+    {
+        public const string Idle = "Idle";
+        public const string Move = "Move";
+    }
+}
